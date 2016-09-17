10cd46b455879280eb7d09fc660e7529f6906854
Glen Holcomb <DamnBigMan@gmail.com>
2016-09-16 20:55:49 -0600

    Add `credo`

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Canary.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:hound, "~> 1.0"}]
+     {:hound, "~> 1.0"},
+     {:credo, "~> 0.4.11", only: [:dev, :test]}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
